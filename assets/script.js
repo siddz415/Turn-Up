a1710806fcd3b51b9d95c0115a20d2f6d145f9f2
dirtnasty jones <jeremymccarter27@gmail.com>
2023-01-30 21:54:18 -0800

    things are displaying in proper order, need to add clear after next search

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,17 @@ const settings = {
 	}
 };
 
+
+
+
+
 const optionsTwo = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '142b74e2a0msh1c7996bc1656b54p1c1fcajsnbf29db333a4d',
 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
+
 };
 async function artistBio2(artistId) {
 	
@@ -58,28 +63,73 @@ function spotifyApi() {
 			const bandName = data.artists.items[0].data.profile.name
 			console.log(bandName);
 			const trackData = data.tracks.items
+			console.log(trackData)
+			var listTitle = document.createElement("h4")
+			listTitle.innerHTML="Top songs"
+			document.getElementById("About").appendChild(listTitle)
+
 			for (let index = 0; index < 3; index++) {
-				const track = trackData[index].data;
-				console.log(track)
+				const track = trackData[index].data.name;
+				console.log(trackData[index].data.name)
+
+				var trackList = document.createElement("li")
+				trackList.innerHTML=track
+				
+				
+				
+				
+				document.getElementById("About").appendChild(trackList)
+				
+
+			//const list = document.getElementById("About");
+ 
+			//data.forEach((track)=>{
+  			//var li = document.createElement("li");
+ 			// li.innerText = track;
+  			
+//})
+		//list.appendChild(li);		
+
 			}
 
 		//	console.log(data.tracks.items[0].data.name)
 		//	console.log(data.tracks.items[1].data.name)
 		//	console.log(data.tracks.items[2].data.name)
-		const playlistData = data.playlists.items
+		var albumTitle = document.createElement("h4")
+		albumTitle.innerHTML="Top Albums"
+		document.getElementById("About").appendChild(albumTitle)
+		const albumData = data.albums.items
 		for (let index = 0; index < 3; index++) {
-			const playlist = playlistData[index].data;
-			console.log(playlist)
+			const albums = albumData[index].data.name;
+			console.log(albums)
+			var albumList = document.createElement("li")
+		
+			albumList.innerHTML=albums
+
+			
+			
+			
+			document.getElementById("About").appendChild(albumList)
+
+
 		}
-		//	console.log(data.playlists.items[0].data.name)
-		//	console.log(data.playlists.items[1].data.name)
-		//	console.log(data.playlists.items[2].data.name)
+
+		//	console.log(data.albums.items[0].data.name)
+		//	console.log(data.albums.items[1].data.name)
+		//	console.log(data.albums.items[2].data.name)
 			artistId = data.artists.items[0].data.uri.substring(15)
 			const bio = await artistBio2(artistId)
 			console.log(bio);
 			const bioText =  bio.data.artist.profile.biography.text
 			console.log(bioText)
-	
+			var bioTitle = document.createElement("h4")
+			var bioBox = document.createElement("p")
+			bioTitle.innerHTML = "Artist Bio"
+			bioBox.innerHTML = bioText
+			 document.getElementById("About").appendChild(bioTitle)
+			 document.getElementById("About").appendChild(bioBox)
+			 document.getElementById("About").appendChild(bio)
+				
 					//document.getElementById("artistdisplay").innerHTML=data.artists.items[0].data.profile.name
 
 					
